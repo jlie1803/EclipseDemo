a9935e00d80aa8528376b26d111f8f4d06a9826d
Jie <45865916+jlie1803@users.noreply.github.com>
2020-04-16 19:39:09 -0500

    Update EclipseDemo.java

--- EclipseDemo/src/EclipseDemo.java
+++ EclipseDemo/src/EclipseDemo.java
@@ -4,7 +4,7 @@ public class EclipseDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("My change");
+		System.out.println("Teammate change");
 
 
 }
