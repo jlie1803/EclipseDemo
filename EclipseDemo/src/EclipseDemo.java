b9f1401e0250a2748c8e0e399cc38500e96a911e
Jie <45865916+jlie1803@users.noreply.github.com>
2020-04-16 19:42:01 -0500

    Update EclipseDemo.java

--- EclipseDemo/src/EclipseDemo.java
+++ EclipseDemo/src/EclipseDemo.java
@@ -4,11 +4,6 @@ public class EclipseDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-<<<<<<< HEAD
-		System.out.println("Jie change");
-=======
 		System.out.println("Teammate change");
->>>>>>> branch 'master' of https://github.com/jlie1803/EclipseDemo.git
-
 
 }
